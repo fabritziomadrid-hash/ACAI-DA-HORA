4b1344455f34de3c04c254de4f9514386ab6df58
fabritziomadrid-hash <fabritzio.madrid@escola.pr.gov.br>
2026-09-24 10:23:26 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+/*
+ * Configurações da loja. Este arquivo é público quando publicado no GitHub Pages.
+ * Nunca coloque aqui senhas, chaves secretas ou a chave service_role do Supabase.
+ */
+const LOJA_CONFIG = Object.freeze({
+  whatsapp: "5541987026386", // DDI + DDD + número, somente dígitos
+  storeAddress: "PREENCHA AQUI O ENDEREÇO COMPLETO DA LOJA",
+  storeMapUrl: "https://maps.google.com/",
+  instagramUrl: "https://www.instagram.com/SEU_USUARIO/",
+  supabaseUrl: "", // Ex.: https://SEU-PROJETO.supabase.co
+  supabaseAnonKey: "", // Chave pública (anon/publishable), nunca service_role
+  databaseTable: "pedidos"
+});
