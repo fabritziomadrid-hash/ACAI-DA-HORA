e05dc66f750881a83afc9cb481eb923c3295f5a7
fabritziomadrid-hash <fabritzio.madrid@escola.pr.gov.br>
2026-09-24 10:45:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.LOJA_CONFIG = Object.freeze({
   whatsapp: "5541987026386",
   storeAddress: "Rua Mário Gasparin, 1042 - Sítio Cercado, Curitiba",
   storeMapUrl: "https://maps.app.goo.gl/JWHzdoPEsV7i5Xjd9",
-  instagramUrl: "https://www.instagram.com/acaudah.ora/",
+  instagramUrl: "https://www.instagram.com/acaidah.ora/",
   supabaseUrl: "",
   supabaseAnonKey: "",
   databaseTable: "pedidos"
