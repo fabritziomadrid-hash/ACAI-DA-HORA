4bfd80b454ac1af1b97eb58cda5bfd5ce0167c76
fabritziomadrid-hash <fabritzio.madrid@escola.pr.gov.br>
2026-09-24 13:38:24 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
+/*
+ * Configurações públicas da loja para o GitHub Pages.
+ * Nunca coloque aqui senhas, chaves secretas ou a chave service_role do Supabase.
+ */
 window.LOJA_CONFIG = Object.freeze({
-  whatsapp: "5541987026386",
+  whatsapp: "5541987026386", // DDI + DDD + número, somente dígitos
   storeAddress: "Rua Mário Gasparin, 1042 - Sítio Cercado, Curitiba",
   storeMapUrl: "https://maps.app.goo.gl/JWHzdoPEsV7i5Xjd9",
   instagramUrl: "https://www.instagram.com/acaidah.ora/",
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://hqecsfbtwuruirnlfabl.supabase.co",
+  supabaseAnonKey: "sb_publishable_syLjj0RW8HHyhiOLkzWlnw_oZWWw9Xh", // Chave pública, nunca service_role
   databaseTable: "pedidos"
-} );
-
+});
