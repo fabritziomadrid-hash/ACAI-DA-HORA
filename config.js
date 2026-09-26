ccf05bc5e4ecc6e97d010f91144ee0aebee00503
fabritziomadrid-hash <fabritzio.madrid@escola.pr.gov.br>
2026-09-26 16:10:19 -0300

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,16 @@ window.LOJA_CONFIG = Object.freeze({
   storeAddress: "Rua Mário Gasparin, 1042 - Sítio Cercado, Curitiba",
   storeMapUrl: "https://maps.app.goo.gl/JWHzdoPEsV7i5Xjd9",
   instagramUrl: "https://www.instagram.com/acaidah.ora/",
+  businessTimeZone: "America/Sao_Paulo",
+  businessHours: {
+    domingo: [{ open: "15:00", close: "21:00" }],
+    segunda: [],
+    terca: [{ open: "16:00", close: "21:00" }],
+    quarta: [{ open: "16:00", close: "21:00" }],
+    quinta: [{ open: "15:00", close: "21:00" }],
+    sexta: [],
+    sabado: [{ open: "15:00", close: "21:00" }]
+  },
   supabaseUrl: "https://hqecsfbtwuruirnlfabl.supabase.co",
   supabaseAnonKey: "sb_publishable_syLjj0RW8HHyhiOLkzWlnw_oZWWw9Xh", // Chave pública, nunca service_role
   databaseTable: "pedidos"
